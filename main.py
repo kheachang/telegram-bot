1ba6dc804075d08fb48123b3d6ebd75e3e38ff93
Khea Chang <81448949+kheachang@users.noreply.github.com>
2021-08-09 16:37:26 -0700

    create telethon client

---
+++ main.py
@@ -0,0 +1,15 @@
+from telethon import TelegramClient, sync
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+api_id = 7396609
+api_hash = '83a3c2a484499d8ca509fa995cb8722e'
+
+client = TelegramClient('session_name', api_id, api_hash).start()
+
+# Getting information about yourself
+me = client.get_me()
+print(me.stringify())
+
+# Sending a message (you can use 'me' or 'self' to message yourself)
+# client.send_message('username', 'Hello World from Telethon!')
